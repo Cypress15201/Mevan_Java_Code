ceb6fce6c71f282ff3d986f5b5f048f26d265aa4
rajee <rajee@host.docker.internal>
2025-03-30 23:21:25 -0400

    Initial Commit

---
+++ CodeCE/src/main/java/com/sample/java/Sample.java
@@ -0,0 +1,18 @@
+package com.sample.java;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class Sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is Test World!" );
+    }
+}
+
